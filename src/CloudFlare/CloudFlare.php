3fb8a018972f442feed4b1b691090f0e9a3b957f
Crit Russell <crit.gfg@gmail.com>
2013-12-17 16:27:05 -0700

    add Visitor method

--- src/CloudFlare/CloudFlare.php
+++ src/CloudFlare/CloudFlare.php
@@ -1,17 +1,39 @@
 <?php
 namespace CloudFlare;
 
+/**
+ * Class CloudFlare
+ *
+ * @package CloudFlare
+ */
 class CloudFlare
 {
-	public static function VisitorIP()
+    /**
+     * @return string
+     */
+    public static function VisitorIP()
 	{
 		// [HTTP_CF_CONNECTING_IP] => xxx.xxx.xxx.xxx
-		return $_SERVER['HTTP_CF_CONNECTING_IP'];
+		return $_SERVER['HTTP_CF_CONNECTING_IP'] ?: "";
 	}
 
-	public static function VisitorCountry()
+    /**
+     * @return string
+     */
+    public static function VisitorCountry()
 	{
 		// [HTTP_CF_IPCOUNTRY] => US
-		return $_SERVER['HTTP_CF_IPCOUNTRY'];
+		return $_SERVER['HTTP_CF_IPCOUNTRY'] ?: "";
 	}
+
+    /**
+     * @return array
+     */
+    public static function Visitor()
+    {
+        // [HTTP_CF_VISITOR] => {"scheme":"https"}
+        $raw = $_SERVER['HTTP_CF_VISITOR'] ?: "{}";
+
+        return json_decode($raw, true) ?: array();
+    }
 }
